0df7b6bef0eb6f29a95c231d378f0a2320877d76
Jeremy Massel <jeremy.massel@gmail.com>
2019-05-23 14:28:01 -0600

    Ensure that login always passes

--- WordPress/src/androidTest/java/org/wordpress/android/support/WPSupportUtils.java
+++ WordPress/src/androidTest/java/org/wordpress/android/support/WPSupportUtils.java
@@ -129,6 +129,17 @@ public class WPSupportUtils {
                 .perform(closeSoftKeyboard());
     }
 
+    public static void populateTextFieldWithin(Integer elementID, String text) {
+        waitForElementToBeDisplayed(elementID);
+
+        onView(allOf(
+                isDescendantOfA(withId(elementID)),
+                withId(R.id.input)
+        ))
+        .perform(replaceText(text))
+        .perform(closeSoftKeyboard());
+    }
+
     public static void populateTextField(ViewInteraction element, String text) {
         waitForElementToBeDisplayed(element);
         element.perform(replaceText(text))
